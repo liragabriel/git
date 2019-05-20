f26a9576293671cabf9a4e449ef7c344501415b6
Gabriel <gabriel.lira@desktop.com.br>
2019-05-20 15:06:48 -0300

    first commit

---
+++ git.py
@@ -0,0 +1,42 @@
+#--------------------------------------------
+#
+# API -> https://github.com/PyGithub/PyGithub
+#
+#          by Gabriel Lira @ 2019
+#
+#--------------------------------------------
+
+from github import Github
+
+class Git:
+
+    def __init__(self, username, password):
+
+        self.username = username
+        self.password = password
+
+    #-----------------------------------------
+    # Retorna todos os repositórios do usuário
+    #-----------------------------------------
+    def lista_repositorios(self):
+
+        g = Github(self.username, self.password)
+
+        for repo in g.get_user().get_repos():
+            print(repo.name)
+
+    #------------------------------------------------
+    # Retorna todos os arquivos dentro do repositório
+    #------------------------------------------------
+    def arquivos_repositorio(self, repo):
+
+        g = Github(self.username, self.password)
+        r = g.get_repo(self.username+'/'+repo)
+
+        contents = r.get_contents('')
+        while len(contents) > 1:
+            file_content = contents.pop(0)
+            if file_content.type == 'dir':
+                contents.extend(r.get_contents(file_content.path))
+            else:
+                print(file_content)
